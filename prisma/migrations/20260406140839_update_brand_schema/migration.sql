18dbdefc64bee6516fcfa430d1e0b825c26c3dea
Ol-create <oluola96@gmail.com>
2026-04-17 15:39:00 +0100

    add package.json

--- prisma/migrations/20260406140839_update_brand_schema/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `contactInfo` on the `Brand` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Brand" DROP COLUMN "contactInfo",
-ADD COLUMN     "description" TEXT;
--- prisma/migrations/20260417045403_change_terms_to_boolean/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `acceptedTermsAt` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "acceptedTermsAt",
-ADD COLUMN     "acceptedTerms" BOOLEAN NOT NULL DEFAULT false;
